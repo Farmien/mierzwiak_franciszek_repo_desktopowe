004d9d7ca8417c317d0deb7adaba20d30924088f
Farmien <franula98765@gmail.com>
2021-05-03 21:45:36 +0200

    Wyglad-ZapisDoPliku-WalidacjaUsernamePassword

--- RejestracjaLogowanie/src/main/java/pl/home/mierzwiak/franciszek/rejestracjalogowanie/JFrame.java
+++ RejestracjaLogowanie/src/main/java/pl/home/mierzwiak/franciszek/rejestracjalogowanie/JFrame.java
@@ -5,6 +5,14 @@
  */
 package pl.home.mierzwiak.franciszek.rejestracjalogowanie;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author frane
@@ -16,6 +24,7 @@ public class JFrame extends javax.swing.JFrame {
      */
     public JFrame() {
         initComponents();
+        
     }
 
     /**
@@ -39,6 +48,7 @@ public class JFrame extends javax.swing.JFrame {
         jPFPConfirmPassword = new javax.swing.JPasswordField();
         JBRejestracja = new javax.swing.JButton();
         jLRejestracja = new javax.swing.JLabel();
+        jLBlad = new javax.swing.JLabel();
         jPLogowanie = new javax.swing.JPanel();
         jLPassword2 = new javax.swing.JLabel();
         jTFEmail2 = new javax.swing.JTextField();
@@ -46,6 +56,7 @@ public class JFrame extends javax.swing.JFrame {
         jLLogowanie = new javax.swing.JLabel();
         jBZaloguj = new javax.swing.JButton();
         jPFPassword2 = new javax.swing.JPasswordField();
+        jLBlad2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,9 +102,12 @@ public class JFrame extends javax.swing.JFrame {
                             .addComponent(jLEmail)
                             .addComponent(jLPassword)
                             .addComponent(jLConfirmPassword)
-                            .addComponent(JBRejestracja, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLRejestracja, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 389, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPRejestracjaLayout.createSequentialGroup()
+                        .addComponent(JBRejestracja, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLBlad, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPRejestracjaLayout.setVerticalGroup(
@@ -117,7 +131,9 @@ public class JFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPFPConfirmPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(JBRejestracja, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPRejestracjaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLBlad, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(JBRejestracja, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -152,14 +168,17 @@ public class JFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPLogowanieLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jTFEmail2)
+                    .addComponent(jPFPassword2)
                     .addGroup(jPLogowanieLayout.createSequentialGroup()
                         .addGroup(jPLogowanieLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLEmail2)
                             .addComponent(jLPassword2)
-                            .addComponent(jLLogowanie, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jBZaloguj, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 389, Short.MAX_VALUE))
-                    .addComponent(jPFPassword2))
+                            .addComponent(jLLogowanie, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPLogowanieLayout.createSequentialGroup()
+                        .addComponent(jBZaloguj, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLBlad2, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPLogowanieLayout.setVerticalGroup(
@@ -175,7 +194,9 @@ public class JFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPFPassword2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jBZaloguj, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPLogowanieLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLBlad2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jBZaloguj, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE))
                 .addGap(167, 167, 167))
         );
 
@@ -200,7 +221,11 @@ public class JFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jTFUsernameActionPerformed
 
     private void JBRejestracjaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBRejestracjaActionPerformed
-        // TODO add your handling code here:
+        try {
+            SprawdzankoRejestracji();
+        } catch (IOException ex) {
+            Logger.getLogger(JFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_JBRejestracjaActionPerformed
 
     private void jTFEmail2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTFEmail2ActionPerformed
@@ -208,7 +233,7 @@ public class JFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jTFEmail2ActionPerformed
 
     private void jBZalogujActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBZalogujActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jBZalogujActionPerformed
 
     /**
@@ -237,18 +262,115 @@ public class JFrame extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new JFrame().setVisible(true);
             }
         });
     }
-
+    
+    private void SprawdzankoRejestracji() throws IOException{
+        //Sprawdzanie Username
+        boolean usernamespr = true;
+        String username = jTFUsername.getText();
+        if(username.length() < 2 || username.length() > 20){
+            usernamespr = false;
+        }else{
+            for(int i=0; i<username.length(); i++){
+                char ch = username.charAt(i);
+                if(ch < 'A' || ch > 'z'){
+                    usernamespr = false;
+                }
+            }
+        }
+        
+        //Sprawdzanie Email
+        boolean emailspr = true;
+        String email = jTFEmail.getText();
+        if(email.contains("@")){
+            String tab[] = email.split("@");
+            if(tab[0] == "@"){
+                emailspr = false;
+            }else{
+                for(int i=0; i<tab[0].length(); i++){
+                    char ch = tab[0].charAt(i);
+                    if(ch < 'A' || ch > 'z'){
+                        emailspr = false;
+                    }
+                }
+                if(tab[1].contains(".")){
+                    String []tab2 = tab[1].split(".");
+                    if(tab2[0].length() < 1 || tab2[1].length() < 1){
+                        emailspr = true;
+                    }
+                }
+            }
+        }else{
+            emailspr = false;
+        }
+        
+        //Sprawdzanie Password
+        boolean passwordspr = true;
+        String password = String.valueOf(jPFPassword.getPassword());
+        for(int i=0; i<password.length(); i++){
+            char ch = password.charAt(i);
+            if(password.length() >= 4){
+                if(!(ch >= 'A' && ch <= 'z' || ch >= '0' && ch <= '9')){
+                    passwordspr = true;
+                }else{
+                    passwordspr = false;
+                }
+            }
+        }
+        //confirm password
+        boolean cpasswordspr = true;
+        String cpassword = String.valueOf(jPFPConfirmPassword.getPassword());
+        if(cpassword.equals(password)){
+            cpasswordspr = false;
+        }else{
+            cpasswordspr = true;
+        }
+        
+        //bladzik
+        String blad = "<html>";
+        if(usernamespr == false){
+            blad += "Niepoprawna nazwa uzytkownika ";
+        }
+        if(emailspr == false){
+            blad += "Niepoprawny email ";
+        }
+        if(passwordspr == true){
+            blad += "Niepoprawne haslo ";
+        }
+        if(passwordspr == true){
+            blad += "Hasla sa rozne";
+        }
+        blad += "</html>";
+        jLBlad.setText(blad);
+        
+        if(usernamespr == true && passwordspr == false && cpasswordspr == false){
+            FileUtils stf = new FileUtils();
+            String text = username+";"+email+";"+password;
+            stf.saveToFile(text);
+        }
+        
+        
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton JBRejestracja;
     private javax.swing.JButton jBZaloguj;
+    private javax.swing.JLabel jLBlad;
+    private javax.swing.JLabel jLBlad2;
     private javax.swing.JLabel jLConfirmPassword;
     private javax.swing.JLabel jLEmail;
     private javax.swing.JLabel jLEmail2;
